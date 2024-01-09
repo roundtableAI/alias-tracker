fdaf83d148660ed588ce6900933651b19aaa7532
mdahardy <mdhardy@princeton.edu>
2024-01-09 17:59:46 -0500

    Updating route

--- call-api.py
+++ call-api.py
@@ -56,7 +56,7 @@ headers = {
 
 # Make request
 response = requests.post(
-    'https://roundtable.ai/.netlify/functions/alias-v011',
+    'https://roundtable.ai/api/alias/v011',
     json=body,
     headers=headers
 )
